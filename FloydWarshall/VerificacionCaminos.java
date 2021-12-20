214e0d1eefa1061f156c5de7b0786e5acea0c4ab
KevinAMamaniC <64039060+KevinAMamaniC2020@users.noreply.github.com>
2021-12-20 14:00:19 -0500

    Agregando Caminos

---
+++ FloydWarshall/VerificacionCaminos.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class VerificacionCaminos{
+	public final static int INF = 9999999,  V = 22;		//V = 22; , V=16; V=16 v=11
+	 
+    public void floydWarshall(int graph[][]){
+        int dist[][] = new int[V][V];
+        int i, j, k;
+
+        for (i = 0; i < V; i++)
+            for (j = 0; j < V; j++)
+                dist[i][j] = graph[i][j];
+        for (k = 0; k < V; k++)
+        {
+            for (i = 0; i < V; i++)
+            {
+                for (j = 0; j < V; j++)
+                {
+                    if (dist[i][k] + dist[k][j] < dist[i][j])
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+        printSolution(dist);
+    }
+ 
+    public void printSolution(int dist[][]){
+        System.out.println("La mtriz muestra el camino mas corto "+
+                         "de distancias entre un par de vertices");
+        for (int i=0; i<V; ++i)
+        {
+            for (int j=0; j<V; ++j)
+            {
+                if (dist[i][j]==INF)
+                    System.out.print("INF ");
+                else
+                    System.out.print(dist[i][j]+"   ");
+            }
+            System.out.println();
+        }
+    }
+}
